6ec2c1f22a30f27804eb9e0c97ebe1f065a10dab
João Gaidzinski <gaidzinski2110@gmail.com>
2024-06-22 23:21:09 -0300

    Altera padrão de testes de sistema de Junit para Java application

---
+++ src/main/java/entity/Campo.java
@@ -0,0 +1,11 @@
+package entity;
+
+import enums.TipoCampoEnum;
+
+public class Campo {
+	private String xpath;
+	private String id;
+	private String name;
+	private TipoCampoEnum tipo;
+	private String value;
+}
--- src/main/java/selenium/AdicionarResponsavelAction.java
+++ src/main/java/selenium/AdicionarResponsavelAction.java
@@ -2,10 +2,6 @@ package selenium;
 
 import java.time.Duration;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,41 +12,48 @@ import builder.AdicionarResponsavelBuilder;
 import entity.AdicionarResponsavelEntity;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class AdicionarResponsavelActionTest {
+public class AdicionarResponsavelAction {
 	
 	public WebDriver driver;
 	protected Wait<WebDriver> wait;
 	private String baseUrl;
 	
-	@BeforeAll
-	public void start() {
+	public AdicionarResponsavelAction(String url) {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-		baseUrl = "https://8080-projetosiga-sigadocker-thm6nvg4wec.ws-us114.gitpod.io/sigawf/app/responsavel/listar";
+		baseUrl = url;
 	}
 	
-	@BeforeEach
-	public void resetTest() {
+	public void resetTest(String url) {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-		baseUrl = "https://8080-projetosiga-sigadocker-thm6nvg4wec.ws-us114.gitpod.io/sigawf/app/responsavel/listar";
+		baseUrl = url;
 		driver.get(baseUrl);
 	}
 	
-	@Test
-	public void testeCaminhoFeliz() {
+	public void primeiroTeste() {
+		resetTest(baseUrl);
 		AdicionarResponsavelEntity entity = AdicionarResponsavelBuilder.get()
 				.setNome("João")
 				.setDescricao("Organizador de arquivos")
 				.setTipo("Lotacao")
 				.setLotacao("ZZ-LTEST")
 				.build();
-		//TODO Teste automatizado usando instancia acima
-		driver.findElement(By.xpath("teste")).click();
+		runTestByInstance(entity);
 	}
-
+	
+	private void runTestByInstance(AdicionarResponsavelEntity entity) {
+		//TODO Teste automatizado usando instancia acima
+	}
+	
+	
+	public static void main(String[] args) {
+		AdicionarResponsavelAction action = new AdicionarResponsavelAction("https://www.google.com");
+		action.primeiroTeste();
+	}
+	
 }
